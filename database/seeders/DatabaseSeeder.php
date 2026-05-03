830abc7f0f430e3a71865245699f34a4c088aeaf
Mel-Gal <m.galon.561038@umindanao.edu.ph>
2026-05-03 10:21:52 +0800

    Clean up DatabaseSeeder by removing docblock
    Removed commented-out docblock for the run method.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,9 +8,7 @@ use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+   
     public function run(): void
     {
         // 1. Create the Master Admin
